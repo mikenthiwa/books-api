7fec645419a28783b558fd884fa299625b769547
Michael Nthiwa <mike.nthiwa@gmail.com>
2018-04-29 12:08:35 +0300

    Fix to deploy to heroku

--- run.py
+++ run.py
@@ -144,5 +144,5 @@ def reset_password():
 
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT', 5000))
-    app.run("", debug=True, port=8000)
+    port = int(os.environ.get('PORT', 8000))
+    app.run("", debug=True, port=port)
